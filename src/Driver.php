a4213c7b6556524df875d80b779de4f893238d99
josephspurrier <github@josephspurrier.com>
2014-01-12 16:01:06 -0500

    Added GridFS supported
    Added functions for GridFS

--- src/Driver.php
+++ src/Driver.php
@@ -32,21 +32,27 @@ abstract class Driver
 
     /**
      * MongoDB client
-     * @var MongoClient
+     * @var \MongoClient
      */
     private $dbClient;
     
     /**
      * MongoDB collection
-     * @var MongoCollection
+     * @var \MongoCollection
      */
     private $dbCollection;
     
     /**
      * MongoDB instance
-     * @var MongoDB
+     * @var \MongoDB
      */
     private $dbInstance;
+    
+    /**
+     * MongoGridFS instance
+     * @var \MongoGridFS
+     */
+    private $gridFS;
     
     // MongoCursor default options
     private $fields = array();
@@ -100,6 +106,7 @@ abstract class Driver
         // Select the database and collection
         $this->dbCollection = $this->dbClient->selectCollection($database, $collection);
         $this->dbInstance = $this->dbClient->{$database};
+        $this->gridFS = $this->dbInstance->getGridFS();
     }
 
     /**
@@ -235,7 +242,7 @@ abstract class Driver
     }
     
     /**
-     * Update an existing database object
+     * Update records based on a given criteria (updates one document by default)
      * @param array $criteria An array description of the objects to update
      * @param array $new_object An array object with which to update the
      * matching records
@@ -463,5 +470,138 @@ abstract class Driver
         
         // Return the status
         return $status;
+    }
+
+    /**
+     * Stores a file in the database
+     * @param string $filename Name of the file to store
+     * @param array $metadata Other metadata fields to include in the file document
+     * @param Options $options Options for the store (Only WriteConcern)
+     * @return \MongoId Returns the _id of the saved file document
+     */
+    protected function storeFile($filename, array $metadata = array(), Options $options = null)
+    {
+        // Update the options array
+        $options = (is_null($options) ? new Options() : $options);
+        
+        return $id = $this->gridFS->storeFile($filename, $metadata, $options->getArray());
+    }
+        
+    /**
+     * Delete a file from the database
+     * @param \MongoId $id _id of the file to remove
+     * @return boolean Returns if the remove was successfully sent to the database
+     */
+    protected function deleteFile(\MongoId $id)
+    {
+        return $this->gridFS->delete($id);
+    }
+    
+    /**
+     * Drops the files and chunks collections from EVERY collection
+     * @return \SurfStack\MongoDB\Return_Status Returns object with:
+     */
+    protected function dropFileCollection()
+    {
+        $result = $this->gridFS->drop();        
+
+        // Set the status
+        $status = new Return_Status($result);
+        
+        // Return the status
+        return $status;
+    }
+    
+    /**
+     * Retrieve a file from the database
+     * @param \MongoId $id _id of the file to find
+     * @return \MongoGridFSFile | NULL Returns the file, if found, or NULL
+     */
+    protected function getFile(\MongoId $id)
+    {
+        // Get the file
+        $file = $this->gridFS->get($id);
+        
+        // If the file is found
+        if (is_a($file, 'MongoGridFSFile'))
+        {
+            return $file;
+        }
+        // Else the file is not found
+        else
+        {
+            return null;
+        }
+    }
+    
+    /**
+     * Download a file to your computer
+     * @param \MongoGridFSFile $file
+     */
+    protected function downloadFile(\MongoGridFSFile $file)
+    {
+        // Verify the file actually exists before downloading
+        // This is the only way determine if the file exists in the database
+        // or not
+        $tempCollection = $this->dbClient->selectCollection($this->database, 'fs.chunks');
+        $result = $tempCollection->findOne(array(
+            'files_id' => $file->file['_id'],
+        ));
+
+        // If the file is not found
+        if (empty($result))
+        {
+            // Inform the user
+            echo 'File not found';
+            exit;
+        }
+        
+        // Output the file directly to the screen so the user is prompted to
+        // download
+        header('Content-Type: '.$file->file['type']);
+        header('Content-Disposition: attachment; filename='.$file->file['name']);
+        header('Content-Transfer-Encoding: binary');
+        echo $file->getBytes();
+        exit;        
+    }
+    
+    /**
+     * Find matching files in the database
+     * @param array $query The fields for which to search
+     * @param array $fields Fields of the results to return
+     * @return array Returns an array of MongoGridFSFile objets
+     */
+    protected function findFiles(array $query=array(), array $fields=array())
+    {    
+        // Get the MongoCursor of results
+        $result = $this->gridFS->find($query, $fields);
+        
+        // Set the MongoCursor options
+        $result->fields($this->fields);
+        $result->limit($this->limit);
+        $result->skip($this->skip);
+        $result->sort($this->sort);
+        
+        // Return the results
+        if ($result == null || $result == false) return array();
+        // Return the array with integers as the keys
+        return iterator_to_array($result, false);
+    }
+    
+    /**
+     * Find one matching file in the database
+     * @param array | string $query The filename or criteria for which to search
+     * @param array $fields Fields of the results to return
+     * @return \MongoGridFSFile | NULL Returns a MongoGridFSFile or NULL
+     */
+    protected function findOneFile($query=array(), array $fields=array())
+    {
+        // Find
+        $result = $this->gridFS->findOne($query, $fields);
+    
+        // Return the results
+        if ($result == null || $result == false) return null;
+        // Return an object
+        return $result;
     }
 }
